21523a3eb50c73dd5cb9f46d58498756c40e8ec0
felipegavilan <felipegaviilan@gmail.com>
2022-08-02 22:32:08 -0300

    first commit

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,25 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  
+return Number.parseInt(num,2);
+
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+
+  var res='';
+  while(num !=0){
+    res = num % 2 + res;
+    num = Math.floor(num/2);
+  }
+  return res;
+
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
