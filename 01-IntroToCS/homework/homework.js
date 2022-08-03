ac2fd38887a57c02ff680d12c0c6b58edd772a23
felipegavilan <felipegaviilan@gmail.com>
2022-08-03 20:14:25 -0300

    new commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,25 +1,28 @@
-'use strict'
+"use strict";
 
 function BinarioADecimal(num) {
-  
-return Number.parseInt(num,2);
-
+  let binario = String(num).split("").reverse();
+  let decimal = 0;
+  for (let i = 0; i < binario.length; i++) {
+    decimal += Number(binario[i]) * 2 ** i;
+  }
+  return decimal;
+  //return Number.parseInt(num,2);
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
 
-  var res='';
-  while(num > 0){
-    res = num % 2 + res;
-    num = Math.floor(num/2);
+  var res = "";
+  while (num > 0) {
+    res = (num % 2) + res;
+    num = Math.floor(num / 2);
   }
   return res;
-
+  //return num.toString(2);
 }
-
 
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
-}
+};
