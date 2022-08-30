1415f6b5c9d4621dd9cb52ad463a116098a9dbc1
felipegavilan <felipegaviilan@gmail.com>
2022-08-30 00:59:12 -0300

    Algoritmos II

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,18 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if(array.length < 1) return [];
+  let left = [];
+  let right = [];
+  let pivot = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    if(array[i] < pivot) left.push(array[i]);
+    else{
+      right.push(array[i]);
+    }
+  }
+  return [].concat(quickSort(left), pivot, quickSort(right));
 }
 
 function mergeSort(array) {
@@ -14,8 +26,29 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length < 2) return array;
 
+    let mitad = Math.floor(array.length / 2);
+    let left = array.slice(0, mitad);
+    let right = array.slice(mitad, array.length);
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    let merge = function(left, right) {
+      let datos = [];
+
+      while(left.length > 0 && right.length > 0){
+        datos.push(left[0] <= right[0]? left.shift() : right.shift());
+      }
+      return datos.concat(left).concat(right);
+    }
+    return merge(left, right);
 }
+let arr = [9, 5, 2, 6, 10];
+console.log(arr);
+console.log();
+console.log(mergeSort(arr));
 
 // No modificar nada debajo de esta línea
 // --------------------------------
